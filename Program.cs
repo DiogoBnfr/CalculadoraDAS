6cf023a5979fa855f62265adb1437797e39b647d
vaeep <diogobonofre@gmail.com>
2023-09-12 22:44:55 -0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+using System.Diagnostics.CodeAnalysis;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Fórmula do cálculo DAS:
+        // (faturamento últimos 12 meses * alíquota Simples Nacional da tabela) – dedução da tabela
+    }
+}
